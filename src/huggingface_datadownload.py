1934067aa7e91ba14619c7c820c99d0715419047
Diana-Cornescu <dianacornescu@outlook.com>
2026-04-10 11:16:42 -0700

    add download function via api requests

--- src/huggingface_datadownload.py
+++ src/huggingface_datadownload.py
@@ -2,7 +2,7 @@ import argparse
 import requests
 from tqdm import tqdm
 from pathlib import Path
-
+import os
 # pip install requests tqdm
 
 def parse_args():
@@ -35,16 +35,36 @@ def file_name_source_map(base_url, subset, meta, reviews):
     return files
 
 
-def download(url, output, filename):
-    pass
+def download_request(specific_url, output, filename):
+    fullpath = os.path.join(output, filename)
+
+    if fullpath.exists(): # prevent from a taxing re-download
+        print(f"Already exists, skipped: {filename}")
+        return
+
+    print(f"Downloading: {filename}")
+    request = requests.get(specific_url, stream=True) #
+    request.raise_for_status()
+
+    # Following code below that handles request-downloads is from Claude as it's a nice-to-have and out of scope of assignment
+    total = int(request.headers.get("content-length", 0))
+    with open(fullpath, "wb") as f, tqdm(
+        total=total, unit="B", unit_scale=True, unit_divisor=1024, desc=fullpath.name
+    ) as bar:
+        for chunk in request.iter_content(chunk_size=1024 * 1024):  # 1 MB chunks
+            f.write(chunk)
+            bar.update(len(chunk))
+
+    print(f"Saved: {filename} in {output}")
+
 
 if __name__ == "__main__":
     args = parse_args()
     out_dir = Path(args.out_dir)
     out_dir.mkdir(parents=True, exist_ok=True) # in case someone just deletes the whole folder
 
-    files = file_name_source_map(args.subset, args.meta, args.reviews)
-    for filename, url in files.items():
-        print(f"Downloading: {filename}")
-        download(url, output, filename)
+    files_map = file_name_source_map(args.base_url,args.subset, args.meta, args.reviews)
+    for filename, specific_url in files_map.items():
+        print(f"Request to download: {filename}")
+        download_request(specific_url, out_dir, filename)
     print("All raw files downloaded")
