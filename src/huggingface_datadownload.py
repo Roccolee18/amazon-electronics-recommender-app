c86ae9843e348fc12bc0df3e683bd5656a12ead4
Diana-Cornescu <dianacornescu@outlook.com>
2026-04-10 11:02:50 -0700

    add main for-loop for each dataset download

--- src/huggingface_datadownload.py
+++ src/huggingface_datadownload.py
@@ -1,32 +1,50 @@
 import argparse
+import requests
+from tqdm import tqdm
 from pathlib import Path
-from datasets import load_dataset
 
-
-
+# pip install requests tqdm
 
 def parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument("--out-dir", 
-                   default="data/raw")
-    p.add_argument("--meta",
-                   default=True,   
-                   help="Whether to download 'meta' source file. Default True")
-    p.add_argument("--reviews", 
-                   default=True,   
-                   help="Whether to download 'meta' source file. Default True")
+    p.add_argument("--base_url",  
+                   default="https://mcauleylab.ucsd.edu/public_datasets/data/amazon_2023/raw", 
+                   help="Download base URL source")
+    p.add_argument("--out-dir",  
+                   default="data/raw", 
+                   help="Directory to save downloaded files.")
+    p.add_argument("--subset",   
+                   default="Electronics", 
+                   help="Category subset to download (e.g. Electronics, Books).")
+    p.add_argument("--meta",     
+                   action=argparse.BooleanOptionalAction,  #Mentioned by Clause to properly parse True/False flag for downloading 
+                   default=True, 
+                   help="Download the meta file.")
+    p.add_argument("--reviews",  
+                   action=argparse.BooleanOptionalAction, 
+                   default=True, 
+                   help="Download the reviews file.")
     return p.parse_args()
 
+def file_name_source_map(base_url, subset, meta, reviews):
+    files = {}
+    if reviews:
+        files[f"{subset}.jsonl.gz"] = f"{base_url}/review_categories/{subset}.jsonl.gz" # to match the same file naming as if manually downloaded from website
+    if meta:
+        files[f"meta_{subset}.jsonl.gz"] = f"{base_url}/meta_categories/meta_{subset}.jsonl.gz" # to match the same file naming as if manually downloaded from website
+    return files
 
 
-def main():
+def download(url, output, filename):
+    pass
+
+if __name__ == "__main__":
     args = parse_args()
-    output  = Path(args.out_dir)
+    out_dir = Path(args.out_dir)
+    out_dir.mkdir(parents=True, exist_ok=True) # in case someone just deletes the whole folder
 
-    meta_data = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_meta_Electronics", 
-                       split="full", 
-                       trust_remote_code=True)
-
-    review_data = load_dataset("McAuley-Lab/Amazon-Reviews-2023",
-                        "raw_review_Electronics", 
-                        trust_remote_code=True)
+    files = file_name_source_map(args.subset, args.meta, args.reviews)
+    for filename, url in files.items():
+        print(f"Downloading: {filename}")
+        download(url, output, filename)
+    print("All raw files downloaded")
