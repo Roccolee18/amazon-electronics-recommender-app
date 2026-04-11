82be53cb7d90f40ba5e71cb7aba131ab24f272d1
Diana-Cornescu <dianacornescu@outlook.com>
2026-04-10 21:31:42 -0700

    add v1 of create documents script

---
+++ src/create_documents.py
@@ -0,0 +1,41 @@
+from utils import *
+
+import argparse
+from pathlib import Path
+import duckdb
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("--input",   
+                   default="data/processed/merged_subset.parquet")
+    p.add_argument("--output", 
+                   default="data/processed/product_documents.parquet")
+    args = p.parse_args()
+
+    input_path = Path(args.input)
+    output_path = Path(args.output)
+
+    # currently just concats title, main_category, and features (after expanding the array)
+    # foundation of query supplied by Claude - including the .as_posix() syntax for paths to work with duckdb window/copy command
+    QUERY = f"""
+    SELECT
+        parent_asin,
+        ANY_VALUE(title) AS product_title,
+        CONCAT_WS(' ',
+            ANY_VALUE(main_category), -- fine b/c identical across all duplicated rows
+            ANY_VALUE(title),
+            -- flatten the features array into a single space-separated string
+            ARRAY_TO_STRING(ANY_VALUE(features), ' ')
+        ) AS document
+    FROM read_parquet('{input_path.as_posix()}')
+    WHERE parent_asin IS NOT NULL
+    GROUP BY parent_asin
+    """
+    
+    con = duckdb.connect()
+    con.execute(
+        f"COPY ({QUERY}) TO '{output_path.as_posix()}' (FORMAT PARQUET, COMPRESSION ZSTD)",
+    )
+
+if __name__ == "__main__":
+    main()
